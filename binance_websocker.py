33500665dff9358204e1f6f549de2d6a056ffc12
EloWeld <51724400+EloWeld@users.noreply.github.com>
2024-05-31 22:34:59 +0300

    Another attermpt

--- binance_websocker.py
+++ binance_websocker.py
@@ -4,7 +4,7 @@ import websocket
 import threading
 import pandas as pd
 import time
-from datetime import datetime, timedelta
+import datetime
 import requests
 import loguru
 import ccxt
@@ -25,16 +25,16 @@ lock = threading.Lock()
 
 def send_webhook(setts, symbol, data, minute):
     url = setts['pump_webhook' if data['type'] == 'pump' else 'dump_webhook']
-    data_template = setts['pump_data' if data['type']
-                          == 'pump' else 'dump_data']
+    data_template: str = setts['pump_data' if data['type'] == 'pump' else 'dump_data']
     data = data_template.replace('{{ticker}}', symbol)
     try:
-        r = requests.post(
-            url, headers={'Content-Type': "application/json"}, data=data)
+        r = requests.post(url, headers={'Content-Type': "application/json"}, data=data)
         if r.status_code != 200:
-            add_journal({"type": "error", "message": r.text, "symbol": symbol, "created_at": minute})
+            add_journal({"type": "error", "message": r.text,
+                        "symbol": symbol, "created_at": minute})
     except Exception as e:
-        add_journal({"type": "error", "message": str(e), "symbol": symbol, "created_at": minute})
+        add_journal({"type": "error", "message": str(
+            e), "symbol": symbol, "created_at": minute})
 
 
 def reload_settings():
@@ -53,7 +53,7 @@ def add_journal(data):
         lines = f.readlines()
 
     # Проверка на дублирование
-    nowd = datetime.now()
+    nowd = datetime.datetime.now()
     now = (int(nowd.timestamp()) // 60) * 60
     log_entries = []
     for line in lines:
@@ -64,31 +64,32 @@ def add_journal(data):
         log_entries.append(log_entry)
         if ('created_at' not in log_entry) or ('symbol' not in log_entry):
             continue
-        log_time = datetime.strptime(log_entry["created_at"], "%Y-%m-%d %H:%M:%S")
-        if log_entry["symbol"] == data["symbol"] and log_entry["type"] == data["type"] and datetime.now() - log_time < timedelta(minutes=settings['check_per_minutes']):
+        log_time = datetime.datetime.strptime(log_entry["created_at"], "%Y-%m-%d %H:%M:%S")
+        if log_entry["symbol"] == data["symbol"] and log_entry["type"] == data["type"] and datetime.datetime.now() - log_time < datetime.timedelta(minutes=settings['check_per_minutes']):
             return  # Запись уже существует, не добавляем дубликат
 
     # Добавление новой записи
     lines.append(json.dumps(data, ensure_ascii=False, default=str) + "\n")
     if data['type'] != "error":
         send_webhook(settings, data['symbol'], data, now)
-        
+
     if data['type'] == "pump":
         spam_all(f"<b>🟢 Новый ПАМП!</b>\n"
-             f"🪙 Монета: <code>{data['symbol']}</code> <a href='https://www.coinglass.com/tv/Binance_{data['symbol']}'>ССЫЛКА</a>\n"
-             f"🎯 Биржа/Мод: <code>{data['exchange']}</code>\n"
-             f"📈 Изменение: <code>{data['change_amount']}</code> за <code>{data['interval']}</code> минут(-ы)\n"
-             f"🌐 Сайт: {settings['domain']}\n"
-             f"📣 Сигналов за сутки: {len([x for x in log_entries if datetime.strptime(x['created_at'], '%Y-%m-%d %H:%M:%S') > datetime(nowd.year, nowd.month, nowd.day)])}"
-             )
+                 f"🪙 Монета: <code>{data['symbol']}</code> <a href='https://www.coinglass.com/tv/Binance_{data['symbol']}'>ССЫЛКА</a>\n"
+                 f"🎯 Биржа/Мод: <code>{data['exchange']}</code>\n"
+                 f"📈 Изменение: <code>{data['change_amount']}</code> за <code>{data['interval']}</code> минут(-ы)\n"
+                 f"🌐 Сайт: {settings['domain']}\n"
+                 f"📣 Сигналов за сутки: {len([x for x in log_entries if datetime.datetime.strptime(x['created_at'], '%Y-%m-%d %H:%M:%S') > datetime.datetime(nowd.year, nowd.month, nowd.day)])}"
+                 )
     elif data['type'] == "dump":
         spam_all(f"<b>🔴 Новый ДАМП!</b>\n"
-                f"🪙 Монета: <code>{data['symbol']}</code> <a href='https://www.coinglass.com/tv/Binance_{data['symbol']}'>ССЫЛКА</a>\n"
-                f"🎯 Биржа/Мод: <code>{data['exchange']}</code>\n"
-                f"📉 Изменение: <code>-{data['change_amount']}</code> за <code>{data['interval']}</code> минут(-ы)\n"
-                f"🌐 Сайт: {settings['domain']}\n"
-                f"📣 Сигналов за сутки: {len([x for x in log_entries if datetime.strptime(x['created_at'], '%Y-%m-%d %H:%M:%S') > datetime(nowd.year, nowd.month, nowd.day)])}"
-                )
+                 f"🪙 Монета: <code>{
+                     data['symbol']}</code> <a href='https://www.coinglass.com/tv/Binance_{data['symbol']}'>ССЫЛКА</a>\n"
+                 f"🎯 Биржа/Мод: <code>{data['exchange']}</code>\n"
+                 f"📉 Изменение: <code>-{data['change_amount']}</code> за <code>{data['interval']}</code> минут(-ы)\n"
+                 f"🌐 Сайт: {settings['domain']}\n"
+                 f"📣 Сигналов за сутки: {len([x for x in log_entries if datetime.datetime.strptime(x['created_at'], '%Y-%m-%d %H:%M:%S') > datetime.datetime(nowd.year, nowd.month, nowd.day)])}"
+                 )
     else:
         spam_all(f"<b>⚠️ Странное поведение!</b>\n"
                  f"Данные: <code>{data}</code>")
@@ -103,135 +104,91 @@ def add_journal(data):
 
 
 def update_price(message):
-    global price_history
-    global settings
-    global spot_pairs
-    global futures_pairs
+    global price_history, settings, spot_pairs, futures_pairs
 
     symbol = message['symbol']
     price = message['price']
-    curr_minute = (int(datetime.now().timestamp()) // 60) * 60
-    # print(timestamp)
-    # print(symbol,"-",price, timestamp)
+    curr_minute = (int(datetime.datetime.now().timestamp()) // 60) * 60
+
     with lock:
-        
         MAX_MINUTES = settings['max_save_minutes']
-        N = settings['check_per_minutes']
+        N1 = settings['check_per_minutes']
         N2 = settings['check_per_minutes_mode_2']
         C1 = settings['price_change_percent']
-        
         C2 = settings.get('price_change_trigger_percent', 0)
         COI = settings.get('oi_change_percent', 0)
         CCVD = settings.get('cvd_change_percent', 0)
         CVVC = settings.get('v_volumes_change_percent', 0)
-        
-        
+
         if symbol not in price_history:
-            # If no prices yet
-            price_history[symbol] = []
-            price_history[symbol].append((curr_minute, price))
+            price_history[symbol] = [(curr_minute, price)]
         else:
-            if price_history[symbol][-1][0] == curr_minute:
-                # If we in current minute candle
-                pass
-            else:
-                # If new candle started add open_time+open_price
+            if price_history[symbol][-1][0] != curr_minute:
                 price_history[symbol].append((curr_minute, price))
-                # Сохраняем только последние 20 минут
                 if len(price_history[symbol]) > MAX_MINUTES:
                     price_history[symbol].pop(0)
+            price_history[symbol][-1] = (curr_minute, price)
 
-        price_history[symbol][-1] = (curr_minute, price)
-
-        # print(N, M, MAX_MINUTES)
-
-        if len(price_history[symbol]) > N:
-            old_price = price_history[symbol][-(N+1)][1]
-
+        def calculate_changes(prices, interval):
+            old_price = prices[-(interval+1)][1]
             if settings['use_wicks']:
-                min_price = min([x[1] for x in price_history[symbol][-(N+1):-1]])
-                max_price = max([x[1] for x in price_history[symbol][-(N+1):-1]])
+                min_price = min(x[1] for x in prices[-(interval+1):-1])
+                max_price = max(x[1] for x in prices[-(interval+1):-1])
             else:
-                min_price = old_price
-                max_price = old_price
-            
-            current_price = price_history[symbol][-1][1]
-            change_amount_pump = (float(current_price) - float(min_price)) / float(min_price) * 100
-            change_amount_dump = (float(max_price) - float(current_price)) / float(max_price) * 100
-            # print(change_amount_pump, change_amount_dump, M)
-            # print(old_price, current_price, change_amount)
-            # oi_candles_pump = True
-            # oi_candles_dump = True
-            # if min_oi_candls > 0:
-            #     oi_candles = get_oi_candles(symbol, min_oi_candls)
-            #     oi_candles_pump = all([oi_candles[i] > oi_candles[i] for i in range(len(oi_candles) - 1)])
-            #     oi_candles_dump = all([oi_candles[i] < oi_candles[i] for i in range(len(oi_candles) - 1)])
-                
+                min_price, max_price = old_price, old_price
+            current_price = prices[-1][1]
+            change_amount_pump = (current_price - min_price) / min_price * 100
+            change_amount_dump = (max_price - current_price) / max_price * 100
+            return change_amount_pump, change_amount_dump, min_price, max_price
+
+        def log_and_journal(symbol, change_amount, change_type, mode, min_price, max_price, interval, current_price):
+            loguru.logger.info(f"{symbol} price {change_type.upper()} by {change_amount:.2f}% over the last {interval} minutes; Datetime: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+            s_data = {
+                "exchange": "binance" if mode == "price" else "binance_smooth",
+                "symbol": symbol,
+                "type": change_type,
+                "mode": mode,
+                "change_amount": f"{change_amount:.2f}%",
+                "interval": interval,
+                "old_price": min_price if change_type == "pump" else max_price,
+                "curr_price": current_price,
+                "created_at": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            }
+            try:
+                add_journal(s_data)
+            except Exception as e:
+                loguru.logger.error(f"Error during journal append: {e}, {traceback.format_exc()}")
+
+        if len(price_history[symbol]) > N1:
+            change_amount_pump, change_amount_dump, min_price, max_price = calculate_changes(price_history[symbol], N1)
             if change_amount_pump >= C1 and settings['enable_pump']:
-                loguru.logger.info(f"{symbol} price PUMPED by {change_amount_pump:.2f}% over the last {N} minutes; Datetime: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-                s_data = {"exchange": "binance", "symbol": symbol, "type": "pump", "mode": "price", "change_amount": f"{change_amount_pump:.2f}%",
-                          "interval": N, "old_price": min_price, "curr_price": current_price, "created_at": datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
-                try:
-                    add_journal(s_data)
-                except Exception as e:
-                    loguru.logger.error(f"Error during journal append: {e}, {traceback.format_exc()}")
-
+                log_and_journal(symbol, change_amount_pump, "pump", "price", min_price, max_price, N1, price_history[symbol][-1][1])
             if change_amount_dump >= C1 and settings['enable_dump']:
-                loguru.logger.info(f"{symbol} price DUMPED by {change_amount_dump:.2f}% over the last {N2} minutes Datetime: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-                s_data = {"exchange": "binance", "symbol": symbol, "type": "dump", "mode": "price", "change_amount": f"{change_amount_dump:.2f}%",
-                          "interval": N2, "old_price": max_price, "curr_price": current_price, "created_at": datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
-                try:
-                    add_journal(s_data)
-                except Exception as e:
-                    loguru.logger.error(f"Error during journal append: {e}, {traceback.format_exc()}")
-                    
-        if len(price_history[symbol]) > N2:
-            old_price = price_history[symbol][-(N2+1)][1]
+                log_and_journal(symbol, change_amount_dump, "dump", "price", min_price, max_price, N1, price_history[symbol][-1][1])
 
-            if settings['use_wicks']:
-                min_price = min([x[1] for x in price_history[symbol][-(N2+1):-1]])
-                max_price = max([x[1] for x in price_history[symbol][-(N2+1):-1]])
-            else:
-                min_price = old_price
-                max_price = old_price
-            
-            current_price = price_history[symbol][-1][1]
-            change_amount_pump = (float(current_price) - float(min_price)) / float(min_price) * 100
-            change_amount_dump = (float(max_price) - float(current_price)) / float(max_price) * 100
-            
+        if len(price_history[symbol]) > N2:
+            change_amount_pump, change_amount_dump, min_price, max_price = calculate_changes(
+                price_history[symbol], N2)
             if change_amount_pump >= C2 and settings['enable_pump']:
                 oi = sorted(get_oi_candles(symbol, max(2, N2 // 5)), key=lambda x: x['timestamp'])
-                oi = [float(x['sumOpenInterest']) for x in oi]
-                oi_change = (oi[-1] - oi[0]) / oi[0] * 100
+                oi_values = [float(x['sumOpenInterest']) for x in oi]
+                oi_change = (oi_values[-1] - oi_values[0]) / oi_values[0] * 100
                 if oi_change > COI:
                     cvd_change = get_cvd_change(symbol, N2+1)
                     if cvd_change > CCVD:
                         volumes_change = get_volumes_change(symbol, N2+1)
                         if volumes_change > CVVC:
-                            # Smooth pump/dump activated
-                            loguru.logger.info(f"{symbol} price SMOOTHED PUMPED by {change_amount_pump:.2f}% over the last {N2} minutes; Datetime: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-                            s_data = {"exchange": "binance_smooth", "symbol": symbol, "type": "pump", "mode": "smooth", "change_amount": f"{change_amount_pump:.2f}%", "interval": N, "old_price": min_price, "curr_price": current_price, "created_at": datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
-                            try:
-                                add_journal(s_data)
-                            except Exception as e:
-                                loguru.logger.error(f"Error during journal append: {e}, {traceback.format_exc()}")
-
+                            log_and_journal(symbol, change_amount_pump, "pump", "smooth", min_price, max_price, N2, price_history[symbol][-1][1])
             if change_amount_dump >= C2 and settings['enable_dump']:
                 oi = sorted(get_oi_candles(symbol, max(2, N2 // 5)), key=lambda x: x['timestamp'])
-                oi = [float(x['sumOpenInterest']) for x in oi]
-                oi_change = (oi[-1] - oi[0]) / oi[0] * 100
+                oi_values = [float(x['sumOpenInterest']) for x in oi]
+                oi_change = (oi_values[-1] - oi_values[0]) / oi_values[0] * 100
                 if -oi_change > COI:
                     cvd_change = get_cvd_change(symbol, N2+1)
                     if -cvd_change > CCVD:
                         volumes_change = get_volumes_change(symbol, N2+1)
                         if -volumes_change > CVVC:
-                            # Smooth pump/dump activated
-                            loguru.logger.info(f"{symbol} price SMOOTHED DUMPED by {change_amount_dump:.2f}% over the last {N2} minutes Datetime: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-                            s_data = {"exchange": "binance_smooth", "symbol": symbol, "type": "dump", "mode": "smooth", "change_amount": f"{change_amount_dump:.2f}%", "interval": N, "old_price": max_price, "curr_price": current_price, "created_at": datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
-                            try:
-                                add_journal(s_data)
-                            except Exception as e:
-                                loguru.logger.error(f"Error during journal append: {e}, {traceback.format_exc()}")
+                            log_and_journal(symbol, change_amount_dump, "dump", "smooth", min_price, max_price, N2, price_history[symbol][-1][1])
 
 
 def save_to_csv():
@@ -250,7 +207,7 @@ def save_to_csv():
                     df_list.append(d)
                 df = pd.DataFrame(df_list)
                 df.to_csv(PRICE_SAVE_FILE, index=False)
-                loguru.logger.info(f"Data saved at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+                loguru.logger.info(f"Data saved at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
 
 def get_oi_candles(symbol: str, period):
@@ -269,7 +226,6 @@ def get_oi_candles(symbol: str, period):
         return None
 
 
-
 def get_cvd_change(symbol, period):
     # Запрос агрегированных сделок за последние три минуты
     trades_response = requests.get(f"https://fapi.binance.com/fapi/v1/trades", params={
@@ -277,10 +233,10 @@ def get_cvd_change(symbol, period):
         "limit": 1000,
     })
     trades = trades_response.json()
-    
+
     # Фильтрация сделок за последние три минуты
-    past_trades = [trade for trade in trades if (period-1) * 60 <= (datetime.now() - datetime.fromtimestamp(trade['time']/1000)).seconds < period * 60]
-    current_trades = [trade for trade in trades if (datetime.now() - datetime.fromtimestamp(trade['time']/1000)).seconds <= 60]
+    past_trades = [trade for trade in trades if (period-1) * 60 <= (datetime.datetime.now() - datetime.datetime.fromtimestamp(trade['time']/1000)).seconds < period * 60]
+    current_trades = [trade for trade in trades if (datetime.datetime.now() - datetime.datetime.fromtimestamp(trade['time']/1000)).seconds <= 60]
     # Расчёт CVD 3 минуты назад
     past_cvd = sum(float(trade['qty']) if trade['isBuyerMaker'] else -float(trade['qty']) for trade in past_trades)
 
@@ -294,8 +250,8 @@ def get_volumes_change(symbol, limit=100):
     # Запрос данных ордербука (глубина рынка)
     klines_response = requests.get(f"https://fapi.binance.com/fapi/v1/klines", params={"symbol": symbol, "interval": "1m", "limit": limit})
     volumes = [x[5] for x in sorted(klines_response.json(), key=lambda x: x[0])]
-    past_vol = volumes[0]
-    current_vol = volumes[-1]
+    past_vol = float(volumes[0])
+    current_vol = float(volumes[-1])
 
     return ((current_vol - past_vol) / past_vol) * 100 if past_vol != 0 else float('inf')
 
@@ -322,7 +278,6 @@ def get_spot_prices():
     else:
         loguru.logger.error(f"Error fetching data from Binance API , code: {response.status_code}, data: {response.text}")
         return None
-
 
 
 def tickers_receiver():
