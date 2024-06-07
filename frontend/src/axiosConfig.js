db96d26475879b68ec9046cf56d73bb734490fc4
EloWeld <51724400+EloWeld@users.noreply.github.com>
2024-06-08 02:37:43 +0300

    Update axiosConfig.js

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_DEV_API_BASE_URL,
+    baseURL: process.env.REACT_APP_NODE_ENV === 'dev' ? process.env.REACT_APP_DEV_API_BASE_URL : process.env.REACT_APP_API_BASE_URL,
     withCredentials: true,
 });
 
