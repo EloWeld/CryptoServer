5fb099a077c9d8373f90d617f92274089f708966
EloWeld <51724400+EloWeld@users.noreply.github.com>
2024-06-20 18:41:50 +0300

    Update check_prices.py

--- backend/app/bacground_tasks/check_prices.py
+++ backend/app/bacground_tasks/check_prices.py
@@ -72,16 +72,16 @@ def check_prices(app):
                     # Creates reversal position of pump and otherwise
                     if log.type == 'pump' and curr_price <= threshold_price:
                         # Filter enable flags
-                        if log.exchange == "rapid" and not settings.reverse_rapid_enable_pump:
+                        if "rapid" in log.exchange and not settings.reverse_rapid_enable_pump:
                             continue
-                        if log.exchange == "smooth" and not settings.reverse_smooth_enable_pump:
+                        if "smooth" in log.exchange and not settings.reverse_smooth_enable_pump:
                             continue
                         send_reverse_webhook(settings, log.symbol, curr_price, 'dump', logged_price, (threshold_price - logged_price) / logged_price * 100, exchange=log.exchange)
                     elif log.type == 'dump' and curr_price >= threshold_price:
                         # Filter enable flags
-                        if log.exchange == "rapid" and not settings.reverse_rapid_enable_dump:
+                        if "rapid" in log.exchange and not settings.reverse_rapid_enable_dump:
                             continue
-                        if log.exchange == "smooth" and not settings.reverse_smooth_enable_dump:
+                        if "smooth" in log.exchange and not settings.reverse_smooth_enable_dump:
                             continue
                         send_reverse_webhook(settings, log.symbol, curr_price, 'pump', logged_price, (threshold_price - logged_price) / logged_price * 100, exchange=log.exchange)
                 time.sleep(5)  # Частота проверки
