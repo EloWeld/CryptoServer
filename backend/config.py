e91cb15571beed61e47928cb0361487eb35d5f59
EloWeld <51724400+EloWeld@users.noreply.github.com>
2024-06-27 16:47:34 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -10,6 +10,11 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://username:password@localhost/db_name')
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'pool_size': 20,
+        'max_overflow': 30,
+        'pool_recycle': 3600
+    }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SETTINGS_FILE = 'settings_new.json'
